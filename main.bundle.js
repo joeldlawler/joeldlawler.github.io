ad9b80cbcdbf57de9ba8393cb1aa49280d3a3232
jdlawler <joellawler@gmail.com>
2017-08-02 08:01:38 -0500

    Add Dynamic SVG - Step 1

--- main.bundle.js
+++ main.bundle.js
@@ -34,7 +34,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"svg-container\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 600 600\">\n      <defs>\n        <style>\n          .cls-1 {\n            fill: #fff;\n          }\n\n          .cls-2 {\n            fill: #bcbec0;\n          }\n        </style>\n      </defs>\n      <title>3_boxes</title>\n      <g id=\"back_box\">\n        <rect width=\"600\" height=\"600\" />\n      </g>\n      <g id=\"right_boxes\">\n        <rect class=\"cls-1\" x=\"12\" y=\"12\" width=\"576\" height=\"576\" />\n        <rect class=\"cls-2\" x=\"396.63\" width=\"203.37\" height=\"600\" />\n        <rect class=\"cls-1\" x=\"430.53\" y=\"42.22\" width=\"135.57\" height=\"135.57\" />\n        <rect class=\"cls-1\" x=\"430.53\" y=\"232.22\" width=\"135.57\" height=\"135.57\" />\n        <rect class=\"cls-1\" x=\"430.53\" y=\"422.22\" width=\"135.57\" height=\"135.57\" />\n      </g>\n    </svg>\n</div>"
+module.exports = "<main class=\"container\">\n  <router-outlet></router-outlet>\n</main>"
 
 /***/ }),
 
@@ -58,7 +58,7 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 AppComponent = __decorate([
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_14" /* Component */])({
         selector: 'app-root',
         template: __webpack_require__("../../../../../src/app/app.component.html"),
         styles: [__webpack_require__("../../../../../src/app/app.component.css")]
@@ -73,9 +73,16 @@ AppComponent = __decorate([
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_angular_in_memory_web_api__ = __webpack_require__("../../../../angular-in-memory-web-api/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__services_in_memory_data_service__ = __webpack_require__("../../../../../src/app/services/in-memory-data.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__services_coordinates_service__ = __webpack_require__("../../../../../src/app/services/coordinates.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__routing_routing_module__ = __webpack_require__("../../../../../src/app/routing/routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__index_index_component__ = __webpack_require__("../../../../../src/app/index/index.component.ts");
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -86,25 +93,234 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
+// Imports for loading & configuring the in-memory web api
+
+
+
+
+
+
 var AppModule = (function () {
     function AppModule() {
     }
     return AppModule;
 }());
 AppModule = __decorate([
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["b" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+            __WEBPACK_IMPORTED_MODULE_7__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_9__index_index_component__["a" /* IndexComponent */]
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+            __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+            __WEBPACK_IMPORTED_MODULE_4_angular_in_memory_web_api__["a" /* InMemoryWebApiModule */].forRoot(__WEBPACK_IMPORTED_MODULE_5__services_in_memory_data_service__["a" /* InMemoryDataService */]),
+            __WEBPACK_IMPORTED_MODULE_8__routing_routing_module__["a" /* RoutingModule */]
         ],
-        providers: [],
-        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+        providers: [__WEBPACK_IMPORTED_MODULE_6__services_coordinates_service__["a" /* CoordinatesService */]],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_7__app_component__["a" /* AppComponent */]]
     })
 ], AppModule);
 
 //# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/index/index.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/index/index.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\n<ul>\n  <li *ngFor=\"let person of persons\">\n    <span> {{person.firstname}}</span>\n    <span>{{person.lastname}}</span>\n  </li>\n</ul>\n\n<div class=\"svg-container\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 600 600\">\n      <defs>\n        <style>\n          .whiteBox {\n            fill: #fff;\n          }\n          .greyBox {\n            fill: #bcbec0;\n          }\n          .redBox {\n            fill: #ff0000;\n          }\n        </style>\n      </defs>\n      <title>3_boxes</title>\n      <g id=\"back_box\">\n        <rect width=\"600\" height=\"600\" />\n      </g>\n      <g id=\"right_boxes\">\n        <rect class=\"whiteBox\" x=\"12\" y=\"12\" width=\"576\" height=\"576\" />\n        <rect class=\"greyBox\" x=\"396.63\" width=\"203.37\" height=\"600\" />\n        <rect class=\"whiteBox\" x=\"430.53\" y=\"42.22\" width=\"135.57\" height=\"135.57\" />\n        <rect class=\"whiteBox\" x=\"430.53\" y=\"232.22\" width=\"135.57\" height=\"135.57\" />\n        <rect class=\"whiteBox\" x=\"430.53\" y=\"422.22\" width=\"135.57\" height=\"135.57\" />\n        <svg:rect *ngFor=\"let person of persons\" class=\"redBox\" [attr.x]=\"person.x_coordinate\" [attr.y]=\"person.y_coordinate\" width=\"20\" height=\"20\" />\n      </g>\n    </svg>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/index/index.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__services_coordinates_service__ = __webpack_require__("../../../../../src/app/services/coordinates.service.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return IndexComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var IndexComponent = (function () {
+    function IndexComponent(coordinatesService) {
+        this.coordinatesService = coordinatesService;
+        this.persons = [];
+    }
+    IndexComponent.prototype.getPersons = function () {
+        var _this = this;
+        this.coordinatesService
+            .getPersons()
+            .then(function (persons) { return _this.persons = persons; });
+    };
+    IndexComponent.prototype.ngOnInit = function () {
+        this.getPersons();
+    };
+    return IndexComponent;
+}());
+IndexComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_14" /* Component */])({
+        selector: 'app-index',
+        template: __webpack_require__("../../../../../src/app/index/index.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/index/index.component.css")]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__services_coordinates_service__["a" /* CoordinatesService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__services_coordinates_service__["a" /* CoordinatesService */]) === "function" && _a || Object])
+], IndexComponent);
+
+var _a;
+//# sourceMappingURL=index.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/routing/routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__index_index_component__ = __webpack_require__("../../../../../src/app/index/index.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return RoutingModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_2__index_index_component__["a" /* IndexComponent */] },
+    { path: '**', redirectTo: '' }
+];
+var RoutingModule = (function () {
+    function RoutingModule() {
+    }
+    return RoutingModule;
+}());
+RoutingModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["b" /* NgModule */])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forRoot(routes)
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */]
+        ],
+        declarations: []
+    })
+], RoutingModule);
+
+//# sourceMappingURL=routing.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/services/coordinates.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_toPromise__ = __webpack_require__("../../../../rxjs/add/operator/toPromise.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_toPromise___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_toPromise__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CoordinatesService; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var CoordinatesService = (function () {
+    function CoordinatesService(http) {
+        this.http = http;
+        this.headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Headers */]({ 'Content-Type': 'application/json' });
+        this.personsUrl = 'api/persons'; // URL to web api
+    }
+    CoordinatesService.prototype.getPersons = function () {
+        return this.http.get(this.personsUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    CoordinatesService.prototype.getPerson = function (login) {
+        var url = this.personsUrl + "/" + login;
+        return this.http.get(url)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    CoordinatesService.prototype.handleError = function (error) {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    };
+    return CoordinatesService;
+}());
+CoordinatesService = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["c" /* Injectable */])(),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["c" /* Http */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_http__["c" /* Http */]) === "function" && _a || Object])
+], CoordinatesService);
+
+var _a;
+//# sourceMappingURL=coordinates.service.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/services/in-memory-data.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return InMemoryDataService; });
+var InMemoryDataService = (function () {
+    function InMemoryDataService() {
+    }
+    InMemoryDataService.prototype.createDb = function () {
+        var persons = [
+            { "login": "a1", "displayname": "Bob Ball", "firstname": "Bob", "lastname": "Ball", "office": "NASH 2715B", "department": "Conference Center", "title": "Hostess", "worknum": "(615) 850-1111", "mobilenum": null, "assistant": null, "assistantlogin": null, "assistantextension": null, "assistantfirstname": null, "assistantlastname": null, "extension": "1111", "floor": "27", "y_coordinate": "415", "x_coordinate": "385" },
+            { "login": "a2", "displayname": "Cam Johnson", "firstname": "Cam", "lastname": "Johnson", "office": "NASH 2715C", "department": "Conference Center", "title": "Hostess", "worknum": "(615) 850-2222", "mobilenum": "(615) 222-2222", "assistant": null, "assistantlogin": null, "assistantextension": null, "assistantfirstname": null, "assistantlastname": null, "extension": "2222", "floor": "27", "y_coordinate": "415", "x_coordinate": "430" },
+            { "login": "a3", "displayname": "Marge Simpson", "firstname": "Marge", "lastname": "Simpson", "office": "NASH 2727", "department": "Floater", "title": "Legal Assistant", "worknum": "(615) 850-3333", "mobilenum": null, "assistant": null, "assistantlogin": null, "assistantextension": null, "assistantfirstname": null, "assistantlastname": null, "extension": "3333", "floor": "27", "y_coordinate": "370", "x_coordinate": "200" },
+            { "login": "a4", "displayname": "Tom Hanks", "firstname": "Tom", "lastname": "Hanks", "office": "NASH 2738A", "department": "Reception", "title": "Receptionist", "worknum": "(615) 850-4444", "mobilenum": "(444) 444-4444", "assistant": null, "assistantlogin": null, "assistantextension": null, "assistantfirstname": null, "assistantlastname": null, "extension": "4444", "floor": "27", "y_coordinate": "130", "x_coordinate": "360" },
+            { "login": "a5", "displayname": "Kim Possible", "firstname": "Kim", "lastname": "Possible", "office": "NASH 2711", "department": "Conference Center", "title": "Hospitality Manager", "worknum": "(615) 850-5555", "mobilenum": "(555) 555-5555", "assistant": null, "assistantlogin": null, "assistantextension": null, "assistantfirstname": null, "assistantlastname": null, "extension": "5555", "floor": "27", "y_coordinate": "355", "x_coordinate": "521" }
+        ];
+        return { persons: persons };
+    };
+    return InMemoryDataService;
+}());
+
+//# sourceMappingURL=in-memory-data.service.js.map
 
 /***/ }),
 
